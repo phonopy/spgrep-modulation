6ea1364213ed5e97da9f2461261934da5e225e5e
lan496 <lan496@users.noreply.github.com>
2025-11-10 23:58:29 +0000

    deploy: 0dddbeadd6c885782e40b93210f53b610495ffe3

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.8.dev25+gfeedfca2b',
+    VERSION: '0.2.8.dev27+g0dddbeadd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
